33136f8414295727c5514e6f5413bf7bbaedb608
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-07 15:09:20 +0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!! From Farzeen"); 
+
+    }
+}
+
+//Single Comment
+//Pascal Convention: -> class
+//HelloWorld
+
+//Multi line comment
+/*
+Camel Convention: ->functions
+helloWorld
+
+*/
+
+
