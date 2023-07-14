841105c1081a583e31e54026e5cd8ad1a8d20be2
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-14 12:54:14 +0500

    Add files via upload

---
+++ ResultingDataType.java
@@ -0,0 +1,43 @@
+/*
+**************Resulting Data Type After Arithmetic Operation******************
+byte + short -> int
+short + int -> int
+long + float -> float
+int + float -> float
+int + char -> int
+short + char -> int
+long + double -> double
+float + double -> double
+*/
+public class ResultingDataType {
+    public static void main(String[] args) {
+        System.out.println("Resulting Data Type After Arithmetic Operation");
+        // int a = 3;
+        // // int b = 4;
+        // // float b = 4.5f;
+        // char b = 'c';
+        // int result = a + b;
+        // System.out.println(result);
+
+        // Increment and Decrement
+        // ++ -> Increment
+        // -- -> Decrement
+
+        // int a = 2;
+        // Pre Increment
+        // System.out.println(++a);
+        // Post Increment
+        // System.out.println(a++);
+        // System.out.println(a);
+        // Pre Decrement
+        // System.out.println(--a);
+        // Post Decrement
+        // System.out.println(a--);
+        // System.out.println(a);
+
+        // char a = 'a';
+        char a = 'F';
+        System.out.println(++a);
+
+    }
+}
