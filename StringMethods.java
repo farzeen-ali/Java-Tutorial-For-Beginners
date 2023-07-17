cad963b0284faf792c6b2c8cd481a6d3b86bba19
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-17 14:35:57 +0500

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,62 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        String message = "Hi I'm Farzeen Ali";
+        // String message = new String("Hi I'm Farzeen Ali");
+        // System.out.println(message);
+
+        // Length Method
+        // int stringLength = message.length();
+        // System.out.println(stringLength);
+
+        // LowerCase Method
+        // String lowerCase = message.toLowerCase();
+        // System.out.println(lowerCase);
+
+        // UpperCase Method
+        // String upperCase = message.toUpperCase();
+        // System.out.println(upperCase);
+
+        //Trim Method
+        // String unTrimmedMessage = "        Farzeen           ";
+        // System.out.println(unTrimmedMessage);
+        // String trimmedMessage = unTrimmedMessage.trim();
+        // System.out.println(trimmedMessage);
+
+        //CharAt Method
+        // char charOfIndex = message.charAt(3);
+        // System.out.println(charOfIndex);
+
+        //IndexOf Method
+        // int indexOfCharacter = message.indexOf('r');
+        // int indexOfCharacter = message.indexOf("ee");
+        // int indexOfCharacter = message.indexOf('i',2);
+        // int indexOfCharacter = message.indexOf("ar",2);
+        // System.out.println(indexOfCharacter);
+
+        // Substring Method
+        // String subStringMessage = message.substring(7,14);
+        // System.out.println(subStringMessage);
+
+        // Replace Method
+        // String replaceString = message.replace('r', 'R');
+        // String replaceString = message.replace("li", "LI");
+        // System.out.println(replaceString);
+
+        // StartsWith Method
+        // boolean startsString = message.startsWith("hi");
+        // System.out.println(startsString);
+        // EndsWith Method
+        // boolean endsString = message.endsWith("li");
+        // System.out.println(endsString);
+
+        // Equals Method
+        // String firstName = "Farzeen";
+        // boolean checkName = firstName.equals("farzeen");
+        // System.out.println(checkName);
+        
+        // EqualsIgnoreCase Method
+        // String firstName = "Farzeen";
+        // boolean checkName = firstName.equalsIgnoreCase("farzeen");
+        // System.out.println(checkName);
+    }
+}
