8a6719b2bb49aff8fbe84b4cdd6626c053f02922
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-22 16:15:52 +0500

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,42 @@
+/*****************Arrays************************
+used to store multiple elements(values) in a single variable having same data type;
+**********Syntax***************
+Declaration:
+    dataType [] arrayName;
+Declaration + Memory Allocation:
+    dataType [] arrayName = new dataType[size of an array];
+Declaration + Initialization:
+    dataType [] arrayName = {1elements,2elements,......,N-elements};
+*****Array Indexes Starts from 0 and goes till (n-1) where n is the size of an array
+1--->0
+2--->1
+3--->2
+4--->3
+5-->4
+*/
+public class Array{
+    public static void main(String[] args) {
+        // First Method
+        // int [] age;//Declare
+        // age = new int[6];//Memory Allocate
+        // age[0] = 20;
+        // age[1] = 30;
+        // age[2] = 40;
+        // age[3] = 50;
+        // age[4] = 60;
+        // age[5] = 70;
+
+        // System.out.println(age[3]);
+
+        // Second Method
+        int [] marks = {55,67,45,78,88};
+        System.out.println(marks[3]);
+
+        System.out.println(marks.length);
+
+    }
+}
+
+
+
+
