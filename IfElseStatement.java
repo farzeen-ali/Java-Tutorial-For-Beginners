daea955097b84f36c26dc0a3fca2a5ad36687027
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-07-24 15:18:58 +0500

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,94 @@
+/*****************Conditional Statement*************
+******************If Else Statement*****************
+helps to run a specific block of code according to the condition
+
+Syntax: 
+if(condition)
+{
+    block of code(if the condition is true then this code will be executed)
+}
+else
+{
+    block of code(if the condition is false then this code will be executed)
+}
+
+*/
+// import java.util.Scanner;
+public class IfElseStatement {
+        public static void main(String[] args) {
+        // int age = 19;
+        // if(age>18)
+        // {
+        //     System.out.println("You Can Vote!");
+        // }
+        // else{
+        //     System.out.println("You cannot Vote because you are under 18");
+        // }
+        
+        // Nested if
+        // int a = 3;
+        // int b = 4;
+        // if(a==2)
+        // {
+        //     if(b==3)
+        //     {
+        //         System.out.println("Both Conditions True");
+        //     }
+        //     else{
+        //         System.out.println("A = 2 but B is not equal to 3");
+        //     }
+        // }
+        // else{
+        //     System.out.println("A is not equal to 2");
+        // }
+
+        // If Else If Ladder
+        // int number = 0;
+        // if(number>0)
+        // {
+        //     System.out.println("Number is Positive");
+        // }
+        // else if(number<0)
+        // {
+        //     System.out.println("Number is Negative");
+        // }
+        // else{
+        //     System.out.println("Number is Equal to Zero");
+        // }
+
+
+            //Practice Program
+    //     double percentage;
+    //     System.out.println("Enter Your Percentage: ");
+    //     Scanner sc = new Scanner(System.in);
+    //     percentage = sc.nextDouble();
+
+    //     if(percentage<=100 && percentage>=90)
+    //     {
+    //         System.out.println("Grade A+");
+    //     }
+    //     else if(percentage<90 && percentage>=80)
+    //     {
+    //         System.out.println("Grade A");
+    //     }
+    //     else if(percentage<80 && percentage>=70)
+    //     {
+    //         System.out.println("Grade B");
+    //     }
+    //     else if(percentage<70 && percentage>=60)
+    //     {
+    //         System.out.println("Grade C");
+    //     }
+    //     else if(percentage<60 && percentage>=50)
+    //     {
+    //         System.out.println("Grade D");
+    //     }
+    //     else if(percentage<50 && percentage>=0)
+    //     {
+    //         System.out.println("Grade F! You Are Fail Prhai Likhai me dehaan lgaao");
+    //     }
+    //     else{
+    //         System.out.println("Invalid Percentage");
+    //     }
+       }
+}
