77d6946feef5273247607225553e8054862cc5b5
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-02 13:53:06 +0500

    Add files via upload

---
+++ ObjectOrientedProgramming.java
@@ -0,0 +1,24 @@
+/******************** Object Oriented Programming (OOP) *********************
+One of the most common strategies in programming is to solve an issue by creating objects.
+
+DRY------> Don't Repeat Yourself
+
+************** Class *****************
+Blueprint to create objects.
+contains information to create a valid objects
+
+************** Object *****************
+An object is a member also known as instance of a class.
+Each Object has an identity, behavior, and a state.
+state of object ----> variables
+behavior of object ----> methods (functions)
+Objects are created at runtime from templates(classes).
+
+************** How To Model a Problem In OOPs **************
+Identity----> Class -----> Employee
+State-------> Attributes ----> empName, empAge, empSalary;
+Behavior----> Methods-----> getSalary();
+*/
+public class ObjectOrientedProgramming {
+    
+}
