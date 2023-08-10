3949fdd82611b85371bfb6899ddd669124e9c896
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-10 23:14:20 +0500

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,15 @@
+package myJavaTutorial;
+/****************** Java Packages *******************
+Organized Collection Of Classes.
+
+why we use?
+1)Organization
+2)Avoid Naming Conflict
+3)Access Control
+4)Modularity  
+*/
+public class Package {
+    public static void main(String[] args) {
+        System.out.println("Me Package Ka Dost Hoon");
+    }
+}
