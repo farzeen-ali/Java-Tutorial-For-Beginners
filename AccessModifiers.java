cf9bffa453a6b3180217b449025a18c0838d400f
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2023-08-19 23:04:18 +0500

    Add files via upload

---
+++ AccessModifiers.java
@@ -0,0 +1,25 @@
+/* ****************** Access Modifiers In Java *********************
+
+    Modifier    Class   Package     Subclass    World
+    public       Yes     Yes          Yes        Yes
+    protected    Yes     Yes          Yes        No
+    private      Yes     No           No         No
+    default      Yes     Yes          No         No
+
+
+*/
+package myJavaTutorial;
+class Student{
+    
+}
+
+class Employee{
+
+}
+
+
+public class AccessModifiers {
+    public static void main(String[] args) {
+        System.out.println("Access Modifiers In Java");
+    }
+}
