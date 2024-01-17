f4b7b33d2738553dbd53ea2f7892e3f5eccb16d9
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-01-17 16:32:17 +0500

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,60 @@
+/*
+***************Operators**********************
+Operands: on which operation performed
+Operators: tells type of operation
+Expression: combination of operands and operators
+e.g:
+    4 + 6 = 10
+    (a=5, b=3)
+    a + b = 8
+
+********Types of Operators***************
+1)Arithmetic Operators: +, -, *, /, %, ++, --
+2)Assignment Operators: =, +=, -=
+3)Comparison Operators: ==, >=, <=, >,<  
+4)Logical Operators: &&, ||, !
+*/
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Operators In Java");
+        System.out.println("Arithmetic Operators");
+        // int a = 4;
+        // int b = 2;
+        // int result;
+
+        // result = a + b;
+        // result = a - b;
+        // result = a * b;
+        // result = a / b;
+        //result = a % b; //modulo
+        // System.out.println(result);
+
+        //Increment 
+        // System.out.println(++a); //pre increment
+        // System.out.println(a++); //post increment
+        // System.out.println(a);
+        
+       // Decrement 
+        //System.out.println(--a); //pre Decrement
+        // System.out.println(a--); //post Decrement
+        // System.out.println(a);
+
+        System.out.println("Assignment Operators");
+        // a = a + b; 
+        //a+=b; //shorthand property
+        // System.out.println(a);
+
+        System.out.println("Comparison Operators");
+        // System.out.println(4==5);
+        // System.out.println(4>5);
+        // System.out.println(4<5);
+        // System.out.println(4<=4); //true
+        // System.out.println(4>=8); //false
+
+        System.out.println("Logical Operators");
+        // System.out.println(4>3 && 4<2); //And Operator
+        // System.out.println(4>3 || 4<2); //Or Operator
+        //System.out.println(!(4>2)); //not operator
+
+    }
+}
